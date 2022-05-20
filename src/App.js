62abd78ec14567253bfe0c31be44cd6cdfa95726
Felipe Matos <felipe.tonattomatos@gmail.com>
2022-05-19 22:01:08 -0300

    nova feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
-
+  
 function App() {
+    let send_api = 0;
+    const [horas , setHoras] = useState('');
+    const [views , setViews] = useState('');
+    const [link  , setlink]  = useState('1');
+    var vizualizacao = 0;
+    var time = 0;
+    const handleChange =(e)=>{
+      setHoras(e.target.value);
+    }
+    const handleAgeChange =(e)=>{
+      setViews(e.target.value);
+    }
+    const handleSubmit=(e)=>{
+      let send_api = 1;
+      setlink("2");
+
+      var vizualizacao = views;
+      var time = horas;
+      console.log(time);
+      setHoras('');
+      setViews('');
+      e.preventDefault();
+    }
+     if(send_api == 1){
+          var axios = require('axios');
+    //   axios.post("",{"time" : time, "views" : vizualizacao})
+     }
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className="App-header">
+    <form onSubmit={(e) => {handleSubmit(e)}}>
+    <h2> Gerador de senha</h2>
+        <label >
+          Horas para visualizar:
+        </label><br/>
+        <input type="text" value={horas} required onChange={(e) => {handleChange(e)}} /><br/>
+          {}
+        <label >
+          Quantidade para vizualizar:
+        </label><br/>
+        <input type="text" value={views} required onChange={(e)=> {handleAgeChange(e)}} /><br/>
+            { }
+               <input type="submit" value="Submit"/>
+      <br></br>
+      <text>Link : {link}</text>
+      </form>
+    </header>
     </div>
   );
 }
-
+  
 export default App;
